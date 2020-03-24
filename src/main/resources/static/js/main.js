db92ec9783bea92207ef27ec3541b2e11bb3f7ee
Artem Zaboev <artem.zaboev@yandex.ru>
2020-03-25 02:49:57 +0300

    Revert "Revert "1)Добавил и настроил router, 2)создал App.vue(Главная страница на которую будут проецироваться все остальные компаненты)""
    This reverts commit 57c10c003066932a5b117945266828058486e193.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,19 +1,17 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
-import Profile from 'pages/Profile.vue'
+import router from 'router/router'
+import App from 'pages/App.vue'
 import vuetify from 'plagins/vuetify'
 import 'vuetify/dist/vuetify.min.css'
-
+// import Axios from 'axios'
 
 // Vue.use(Vuetify)
 Vue.use(VueResource);
-
-// new Vue({
-//     el:'#app',
-//     render: a => a(Profile)
-// });
+// Vue.prototype.$http = Axios;
 
 new Vue({
     vuetify,
-    render: a => a(Profile)
+    router,
+    render: a => a(App)
 }).$mount('#app')
