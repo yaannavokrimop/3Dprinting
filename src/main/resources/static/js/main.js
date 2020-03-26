670b0f6cebff42e2edbe65524533ab0dfaa77350
Oleg <55327921+olegnysss@users.noreply.github.com>
2020-03-26 12:42:10 +0300

    Подключил несколько плагинов для фронта, добавил вью для авторизации и регистрации

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,10 +4,14 @@ import router from 'router/router'
 import App from 'pages/App.vue'
 import vuetify from 'plagins/vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 // import Axios from 'axios'
 
 // Vue.use(Vuetify)
 Vue.use(VueResource);
+Vue.use(BootstrapVue)
 // Vue.prototype.$http = Axios;
 
 new Vue({
--- src/main/resources/static/js/router/router.js
+++ src/main/resources/static/js/router/router.js
@@ -2,16 +2,20 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Profile from 'pages/Profile.vue'
 import Main from 'pages/Main.vue'
+import SignUp from 'pages/SignUp.vue'
+import SignIn from 'pages/SignIn.vue'
 
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes=[
     {path:'/',component:Main},
     {path:'/profile',component:Profile},
+    {path:'/register',component:SignUp},
+    {path:'/singin',component:SignIn},
     // {path:'/profile/:id',component:'Profile'},
     // {path:'*',component:'Main'}
-]
+];
 
 export default new VueRouter({
     mode:'history',
